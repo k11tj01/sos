78b09d0f67e4f25ebbfcd8b2523f3affe5107557
Tibin John <Tibin.John11@kzoo.edu>
2013-10-28 14:06:36 -0400

    CPR webview attempt

--- gen/com/comp489/sos/R.java
+++ gen/com/comp489/sos/R.java
@@ -25,23 +25,27 @@ public final class R {
         public static final int medbutton=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
-        public static final int imageButtonMed=0x7f080001;
-        public static final int listView1=0x7f080002;
-        public static final int medical=0x7f080000;
+        public static final int action_settings=0x7f080004;
+        public static final int imageButtonMed=0x7f080002;
+        public static final int listView1=0x7f080003;
+        public static final int medical=0x7f080001;
+        public static final int webView1=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_home_screen=0x7f030000;
-        public static final int activity_medical=0x7f030001;
-        public static final int medical_home_screen=0x7f030002;
+        public static final int activity_cprinstr=0x7f030000;
+        public static final int activity_home_screen=0x7f030001;
+        public static final int activity_medical=0x7f030002;
+        public static final int medical_home_screen=0x7f030003;
     }
     public static final class menu {
-        public static final int home_screen=0x7f070000;
+        public static final int cprinstr=0x7f070000;
+        public static final int home_screen=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int title_activity_cprinstr=0x7f050003;
     }
     public static final class style {
         /** 
